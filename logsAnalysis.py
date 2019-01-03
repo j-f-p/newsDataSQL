d2092f373eca8186a224d6c337b389740f9851d3
JF Padilla <emailreel@gmail.com>
2019-01-02 20:57:33 -0500

    feature: format display of results as expected
    + define a second view for the count calculation
    + add a query to list relevant results from count calculation
    + improve format of text table output
    
    
    Former-commit-id: 42ee921745d3a31e8de401a7328f4245170dd15e

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -7,11 +7,17 @@ def topThreeArticles():
   cursor = conn.cursor()
   cursor.execute("create view av1 as select id, author, title, "
     + "concat('/article/', slug) as path from articles;")
-  cursor.execute( "select av1.path, count(av1.path) as num from "
-    + "av1, "
+  # Note that since slug is unique, according to '\d articles',
+  # path = concat('/article/', slug) is also unique. Thus, first count
+  # in a way to guarantee unique articles are counted.
+  cursor.execute("create view av2 as "
+    + "select av1.path, count(1) as views from av1, "
     + "(select path from log where path like '/article/%') as sub2 "
     + "where av1.path = sub2.path "
-    + "group by av1.path order by num desc limit 3" )
+    + "group by av1.path order by views desc limit 3" )
+  # Then, display the relevant content in the expected order.
+  cursor.execute("select av1.title, av2.views from av2, av1 "
+    + "where av2.path = av1.path order by views desc")
   topArticles = cursor.fetchall()
   conn.close()
   return topArticles
@@ -26,6 +32,10 @@ def topThreeAuthors(raw_content):
   conn.commit()
   conn.close()
 
-print("{0:>32}{1:>32}".format("Article","Views"))
+print
+print("{0:^56}".format("Top 3 Viewed Articles"))
+print
+print("{0:^40}{1:^16}".format("Article","Views"))
 for article in topThreeArticles():
-    print("{0:>32}{1:>32}".format(article[0], article[1]))
+    print("{0:^40}{1:^16}".format(article[0], article[1]))
+print
