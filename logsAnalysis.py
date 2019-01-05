15b62bb3a9d01126f45058b45a8228d7780c2576
JF Padilla <emailreel@gmail.com>
2019-01-04 20:29:43 -0500

    style: update indents to comply with PEP 8
    [style, refactor, prep]
    + update function text argument line continuation indent to comply with
        PEP 8
    + replace stand-alone print with newline escape sequence where convenient
    + add frame for next function to be developed
    
    
    Former-commit-id: 364f4f2328cc11f7ad9af5c4f59cf4819c1dce0c

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -5,21 +5,28 @@ def topThreeArticles():
     """List the most popular three articles of all time"""
     conn = psycopg2.connect("dbname=news")
     cursor = conn.cursor()
+
     # Views here created are temporary. They are not saved to the database.
-    cursor.execute("create view av1 as "
-    + "select id, author, title, concat('/article/', slug) as path "
-    + "from articles;")
+    cursor.execute(
+        "create view av1 as "
+      + "select id, author, title, concat('/article/', slug) as path "
+      + "from articles;")
+
     # Note that since slug is unique, according to '\d articles',
     # path = concat('/article/', slug) is also unique. Thus, view av2 is formed
     # by counting in a way to guarantee unique articles are counted.
-    cursor.execute("create view av2 as "
-    + "select av1.path, count(1) as views from av1, "
-    + "(select path from log where path like '/article/%') as subs "
-    + "where av1.path = subs.path "
-    + "group by av1.path order by views desc limit 3" )
+    cursor.execute(
+        "create view av2 as "
+      + "select av1.path, count(1) as views from av1, "
+      + "(select path from log where path like '/article/%') as subs "
+      + "where av1.path = subs.path "
+      + "group by av1.path order by views desc limit 3" )
+
     # Then, display the relevant content in the expected order.
-    cursor.execute("select av1.title, av2.views from av2, av1 "
-    + "where av2.path = av1.path order by views desc")
+    cursor.execute(
+        "select av1.title, av2.views from av2, av1 "
+      + "where av2.path = av1.path order by views desc")
+
     topArticles = cursor.fetchall()
     conn.close()
     return topArticles
@@ -28,41 +35,55 @@ def topThreeAuthors():
     """List the most popular three authors of all time"""
     conn = psycopg2.connect("dbname=news")
     cursor = conn.cursor()
+
     # See comments in topThreeArticles() about views av1 and av2.
-    cursor.execute("create view av1 as "
-    + "select id, author, title, concat('/article/', slug) as path "
-    + "from articles;")
+    cursor.execute(
+        "create view av1 as "
+      + "select id, author, title, concat('/article/', slug) as path "
+      + "from articles;")
+
     # View av2 is different here in that it includes all the articles.
-    cursor.execute("create view av2 as "
-    + "select av1.path, count(1) as views from av1, "
-    + "(select path from log where path like '/article/%') as subs "
-    + "where av1.path = subs.path "
-    + "group by av1.path order by views desc;" )
+    cursor.execute(
+        "create view av2 as "
+      + "select av1.path, count(1) as views from av1, "
+      + "(select path from log where path like '/article/%') as subs "
+      + "where av1.path = subs.path "
+      + "group by av1.path order by views desc;" )
+
     # Apply a count to av2 joined with av1 grouped by authors
-    cursor.execute("create view av3 as "
-    + "select av1.author, sum(av2.views) as \"Author Views\" "
-    + "from av1, av2 where av1.path = av2.path "
-    + "group by av1.author order by \"Author Views\" desc;" )
-    cursor.execute("select authors.name, av3.\"Author Views\" "
-    + "from authors, av3 "
-    + "where authors.id = av3.author limit 3;")
+    cursor.execute(
+        "create view av3 as "
+      + "select av1.author, sum(av2.views) as \"Author Views\" "
+      + "from av1, av2 where av1.path = av2.path "
+      + "group by av1.author order by \"Author Views\" desc;" )
+
+    cursor.execute(
+        "select authors.name, av3.\"Author Views\" "
+      + "from authors, av3 "
+      + "where authors.id = av3.author limit 3;")
+
     topAuthors = cursor.fetchall()
     conn.close()
     return topAuthors
 
+def significantErrorDays():
+    """List days when more than 1% of requests lead to errors"""
+    conn = psycopg2.connect("dbname=news")
+    cursor = conn.cursor()
+
+    errorProneDays = cursor.fetchall()
+    conn.close()
+    return errorProneDays
+
 def printTopThreeArticles():
-    print
-    print("{0:^56}".format("Top 3 Viewed Articles"))
-    print
+    print("\n{0:^56}\n".format("Top 3 Viewed Articles"))
     print("{0:^40}{1:^16}".format("Article","Views"))
     for article in topThreeArticles():
         print("{0:^40}{1:^16}".format(article[0], article[1]))
     print
 
 def printTopThreeAuthors():
-    print
-    print("{0:^56}".format("Top 3 Viewed Authors"))
-    print
+    print("\n{0:^56}\n".format("Top 3 Viewed Authors"))
     print("{0:^40}{1:^16}".format("Author","Total Article Views"))
     for article in topThreeAuthors():
         print("{0:^40}{1:^16}".format(article[0], article[1]))
