0abd7a02d67882fa16fce823a2a21143763b3847
JF Padilla <emailreel@gmail.com>
2019-01-05 12:57:43 -0500

    refactor: move repeat code into auxiliary function
    [refactor, docs, chore]
    + To comply with DRY principle, move generation of views, common between
        two functions, to a separate function.
    + Give views more simple names.
    + Update comments.
    + Add gitignore file.
    
    
    Former-commit-id: 3d2ad515b6577dc2c12d33edf03d05dd155003b2

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -1,31 +1,40 @@
 # Logs Analysis
+# SQL views here created are temporary. They are not saved to the database.
+
 import datetime, psycopg2
+
+def generateCommonViews(cursor):
+    # These views are common to topThreeArticles() and topThreeAuthors().
+
+    # Create articles view with path instead of slug so that this view can be
+    # joined to a log view with path as the common column.
+    cursor.execute(
+        "create view v1 as "
+      + "select id, author, title, concat('/article/', slug) as path "
+      + "from articles;")
+
+    # Create a view that joins components of the articles and log tables.
+    # Note that since slug is unique, according to '\d articles',
+    # path = concat('/article/', slug) is also unique. Thus, view v2 is formed
+    # by counting in a way to guarantee unique articles are counted.
+    cursor.execute(
+        "create view v2 as "
+      + "select v1.path, count(1) as views from v1, "
+      + "(select path from log where path like '/article/%') as subs "
+      + "where v1.path = subs.path "
+      + "group by v1.path order by views desc" )
 
 def topThreeArticles():
     """List the most popular three articles of all time"""
     conn = psycopg2.connect("dbname=news")
     cursor = conn.cursor()
 
-    # Views here created are temporary. They are not saved to the database.
-    cursor.execute(
-        "create view av1 as "
-      + "select id, author, title, concat('/article/', slug) as path "
-      + "from articles;")
+    generateCommonViews(cursor)
 
-    # Note that since slug is unique, according to '\d articles',
-    # path = concat('/article/', slug) is also unique. Thus, view av2 is formed
-    # by counting in a way to guarantee unique articles are counted.
+    # Collect the relevant content in the expected order.
     cursor.execute(
-        "create view av2 as "
-      + "select av1.path, count(1) as views from av1, "
-      + "(select path from log where path like '/article/%') as subs "
-      + "where av1.path = subs.path "
-      + "group by av1.path order by views desc limit 3" )
-
-    # Then, display the relevant content in the expected order.
-    cursor.execute(
-        "select av1.title, av2.views from av2, av1 "
-      + "where av2.path = av1.path order by views desc")
+        "select v1.title, v2.views from v2, v1 "
+      + "where v2.path = v1.path order by views desc limit 3")
 
     topArticles = cursor.fetchall()
     conn.close()
@@ -36,31 +45,21 @@ def topThreeAuthors():
     conn = psycopg2.connect("dbname=news")
     cursor = conn.cursor()
 
-    # See comments in topThreeArticles() about views av1 and av2.
-    cursor.execute(
-        "create view av1 as "
-      + "select id, author, title, concat('/article/', slug) as path "
-      + "from articles;")
+    generateCommonViews(cursor)
 
-    # View av2 is different here in that it includes all the articles.
+    # Create a view with a request count grouped by authors
+    # from v2 joined with v1.
     cursor.execute(
-        "create view av2 as "
-      + "select av1.path, count(1) as views from av1, "
-      + "(select path from log where path like '/article/%') as subs "
-      + "where av1.path = subs.path "
-      + "group by av1.path order by views desc;" )
+        "create view v3 as "
+      + "select v1.author, sum(v2.views) as \"Author Views\" "
+      + "from v1, v2 where v1.path = v2.path "
+      + "group by v1.author order by \"Author Views\" desc;" )
 
-    # Apply a count to av2 joined with av1 grouped by authors
+    # Collect the relevant content in the expected order.
     cursor.execute(
-        "create view av3 as "
-      + "select av1.author, sum(av2.views) as \"Author Views\" "
-      + "from av1, av2 where av1.path = av2.path "
-      + "group by av1.author order by \"Author Views\" desc;" )
-
-    cursor.execute(
-        "select authors.name, av3.\"Author Views\" "
-      + "from authors, av3 "
-      + "where authors.id = av3.author limit 3;")
+        "select authors.name, v3.\"Author Views\" "
+      + "from authors, v3 "
+      + "where authors.id = v3.author limit 3;")
 
     topAuthors = cursor.fetchall()
     conn.close()
